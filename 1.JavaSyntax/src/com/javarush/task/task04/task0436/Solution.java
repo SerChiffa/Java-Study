abe042f23497bd01ce88c046788ba8dd023c1eaa
SerChiffa <gandebobel@gmail.com>
2018-08-15 17:13:13 +0800

    Рисуем прямоугольник Ввести с клавиатуры два числа m и n. Используя цикл for вывести на экран прямоугольник размером m на n из восьмёрок.

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0436/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task04.task0436;
+
+
+/* 
+Рисуем прямоугольник
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+        rectangle(a, b);
+    }
+
+    public static void rectangle(int m, int n) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("8");
+            }
+            System.out.println();
+        }
+
+    }
+}

fbbce246e5d79477a18aa7be945723865e348f60
SerChiffa <gandebobel@gmail.com>
2018-08-15 17:43:40 +0800

    Треугольник из восьмерок Используя цикл for вывести на экран прямоугольный треугольник из восьмёрок со сторонами 10 и 10.

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0438/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task04.task0438;
+
+/* 
+Рисуем линии
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        for (int a = 1; a < 11; a++) {
+            System.out.print(8);
+        }
+        for (int b = 1; b < 11; b++) {
+            System.out.println(8);
+        }
+
+    }
+}

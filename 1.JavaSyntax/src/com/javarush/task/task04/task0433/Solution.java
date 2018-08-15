826b123096f96587f5c381d9d7f68c302ff7f50b
SerChiffa <gandebobel@gmail.com>
2018-08-15 13:41:04 +0800

    Вывести на экран квадрат из 10х10 букв S используя цикл while

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0433/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task04.task0433;
+
+
+/* 
+Гадание на долларовый счет
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        String a = "SSSSSSSSSS";
+        int b = 1;
+        while(b <= 10) {
+            b++;
+            System.out.println(a);
+        }
+
+    }
+}

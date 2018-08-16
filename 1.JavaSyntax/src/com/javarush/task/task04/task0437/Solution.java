c8da15bc70d33ba3cd12be717b4ab0a18bef0158
SerChiffa <gandebobel@gmail.com>
2018-08-16 16:51:30 +0800

    Суммирование Вводить с клавиатуры числа. Если пользователь ввел -1, вывести на экран сумму всех введенных чисел и завершить программу. -1 должно учитываться в сумме.

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0437/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task04.task0437;
+
+
+/* 
+Треугольник из восьмерок
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        for (int a = 1; a < 11; a++) {
+            for (int b = 0; b < a; b++) {
+                System.out.print("8");
+            }
+            System.out.println();
+        }
+
+    }
+}

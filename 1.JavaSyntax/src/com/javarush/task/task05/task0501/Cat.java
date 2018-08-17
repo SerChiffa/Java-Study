4fe2179d046cc8258759bbf28b75e93713f23b44
SerChiffa <gandebobel@gmail.com>
2018-08-17 16:11:08 +0800

    Создание кота Создать класс Cat. У кота должно быть имя (name, String), возраст (age, int), вес (weight, int), сила (strength, int).

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0501/Cat.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task05.task0501;
+
+/* 
+Создание кота
+*/
+
+public class Cat {
+    public String name;//напишите тут ваш код
+    public int age;
+    public int weight;
+    public int strength;
+
+    public static void main(String[] args) {
+
+    }
+}

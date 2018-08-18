8dc1b4a118fe05399ca3fa88c9e5c8035282b60b
SerChiffa <gandebobel@gmail.com>
2018-08-19 01:55:12 +0800

    Человечки Создать class Person. У человека должно быть имя String name, возраст int age, адрес String address, пол char sex.

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0506/Person.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task05.task0506;
+
+/* 
+Человечки
+*/
+
+public class Person {
+    String name = "Valera";
+    int age = 42;
+    String address = "strit Lenin 5, 34";
+    char sex = 'm';
+
+    public static void main(String[] args) {
+
+    }
+}

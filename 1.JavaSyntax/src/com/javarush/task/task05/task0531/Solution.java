191a742495721d2f9ca527bcf939c9088bafd27a
SerChiffa <gandebobel@gmail.com>
2018-09-02 22:56:54 +0800

    Совершенствуем функциональность Текущая реализация: Программа вводит два числа с клавиатуры и выводит минимальное из них на экран. Новая задача: Программа вводит пять чисел с клавиатуры и выводит минимальное из них на экран.

---
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0531/Solution.java
@@ -0,0 +1,43 @@
+package com.javarush.task.task05.task0531;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Совершенствуем функциональность
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+        int c = Integer.parseInt(reader.readLine());
+        int d = Integer.parseInt(reader.readLine());
+        int e = Integer.parseInt(reader.readLine());
+
+        int minimum = min(a, b, c, d, e);
+
+        System.out.println("Minimum = " + minimum);
+    }
+
+
+    public static int min(int a, int b, int c, int d, int e) {
+        int min1 = a;
+        if (b <= min1) {
+            min1 = b;
+        }
+        if (c <= min1) {
+            min1 = c;
+        }
+        if (d <= min1) {
+            min1 = d;
+        }
+       if (e <= min1){
+            min1 = e;
+        }
+
+        return min1;
+    }
+}

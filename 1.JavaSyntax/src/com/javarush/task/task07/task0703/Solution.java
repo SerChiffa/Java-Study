3ce185a7e81b29578c676893134ad048c0d115b0
SerChiffa <gandebobel@gmail.com>
2018-11-05 14:48:32 +0800

    1. Создать массив на 10 чисел. 2. Ввести с клавиатуры 10 чисел и записать их в массив. 3. Вывести на экран элементы массива в обратном порядке, каждое значение выводить с новой строки.
    Требования:
    •	Программа должна создавать массив на 10 целых чисел.
    •	Программа должна считывать числа для массива с клавиатуры.
    •	Программа должна выводить 10 строчек, каждую с новой строки.
    •	Массив должен быть выведен на экран в обратном порядке.

--- 1.JavaSyntax/src/com/javarush/task/task07/task0703/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0703/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task07.task0703;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 /* 
 Общение одиноких массивов
@@ -8,16 +9,6 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        String[] line = new String[10];
-        int[] num = new int[10];
-        Scanner sc = new Scanner(System.in);
-
-        for (int i = 0; i < 10; i++) {
-            line[i] = sc.nextLine();
-        }
-        for (int i = 0; i < num.length; i++) {
-            num[i] = line[i].length();
-            System.out.println(num[i]);
-        }
+        //напишите тут ваш код
     }
 }

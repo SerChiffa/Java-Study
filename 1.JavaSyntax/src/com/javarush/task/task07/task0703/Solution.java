5d0172f9be817812f5bd98adc965be410e53e9dc
SerChiffa <gandebobel@gmail.com>
2018-11-05 14:06:13 +0800

    1. Создать массив на 10 строк. 2. Создать массив на 10 чисел. 3. Ввести с клавиатуры 10 строк, заполнить ими массив строк. 4. В каждую ячейку массива чисел записать длину строки из массива строк, индекс/номер ячейки которой совпадает с текущим индексом из массива чисел.
    Вывести содержимое массива чисел на экран, каждое значение выводить с новой строки.
    
    Требования:
    •	Программа должна создавать массив на 10 строк.
    •	Программа должна создавать массив на 10 целых чисел.
    •	Программа должна считывать строки для массива с клавиатуры.
    •	Программа должна в массив чисел записать длины строк из массива строк, а затем их вывести на экран.

---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0703/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task07.task0703;
+
+import java.util.Scanner;
+
+/* 
+Общение одиноких массивов
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        String[] line = new String[10];
+        int[] num = new int[10];
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < 10; i++) {
+            line[i] = sc.nextLine();
+        }
+        for (int i = 0; i < num.length; i++) {
+            num[i] = line[i].length();
+            System.out.println(num[i]);
+        }
+    }
+}
